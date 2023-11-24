c653d4df212b9c0b9d8529bf0303607f35f1d83f
Bohdan <ilkivbo16@gmail.com>
2023-11-24 17:56:41 +0200

    Qualifier

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,37 +1,23 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
-
+@Component
 public class MusicPlayer {
-    private List<Music> musicList = new ArrayList<>();
-    private String name;
-    private int volume;
+    private Music music1;
+    private Music music2;
 
-    public String getName() {return name;}
-
-    public void setName(String name) {this.name = name;}
-
-    public int getVolume() {return volume;}
-
-    public void setVolume(int volume) {this.volume = volume;}
-
-    public void setMusicList(List<Music> musicList) {
-        this.musicList = musicList;
-    }
-    //IoC
-   // public MusicPlayer(Music music){this.music = music;}
-
-    public MusicPlayer(){}
-
-    //public void setMusic (Music music){this.music = music;}
-
-    public void playMusic(){
-        for (Music i: musicList) {
-            System.out.println("Playing: " + i.getSong());
-        }
-
+    public MusicPlayer(@Qualifier("rockMusic") Music music1,
+                       @Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
-
+    public String playMusic(){
+        return "Playing: " + music1.getSong() + ", " + music2.getSong();
+    }
 }
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -7,15 +7,11 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
 
-        //Music music = context.getBean("musicBean", Music.class);
-        //MusicPlayer musicPlayer = new MusicPlayer(music);
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
-
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
         context.close();
     }
 }
